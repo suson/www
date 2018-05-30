c734be1ab6420112e5b3364dae9368326e981352
lixusheng <350015235@qq.com>
2018-05-30 20:01:25 +0800

    test

--- fread.php
+++ fread.php
@@ -5,29 +5,43 @@ $user = 'root';
 $pass = '123456';
 $db = 'test';
 
-$txtfile = 'test.txt'; // 读取的文件
+$txtfile = 'test.txt'; // 读取内容的文件
+$title_file = 'test.txt'; // 读取标题的文件
 $file = @fopen($txtfile, 'r');
 
 $table = 'tb_urls'; // 要写入的表名
 $line = 5; // 读取几行当一串内容
-$field = 'body'; // 要写入的字段
 
-
-if (!$file) {
+$title_file_res = @fopen($title_file,'r'); 
+if (!$file || !$title_file_res) {
     die('file open fail');
 } else {
     $pdo = connection($host, $user, $pass, $db, $port);
+    $j = 0;
     while (!feof($file)) {
         $content = array();
+        
         for ($i = 0; $i < $line; $i++) {
             $content[$i] = mb_convert_encoding(fgets($file), "UTF-8", "GBK,ASCII,ANSI,UTF-8");
 //            $content[$i] = fgets($file);
         }
+        $k = 0;
+        $title = '';
+        while (!feof($title_file_res)) {
+            
+            if ($k === $j) {
+                $title = mb_convert_encoding(fgets($title_file_res), "UTF-8", "GBK,ASCII,ANSI,UTF-8");
+                break;
+            }
+            $k++;
+
+        }
+        $j++;
         $content = array_filter($content); //数组去空
         $content_str = implode('', $content);
-        $sql = "INSERT INTO {$table} ({$field})VALUES (:val)";
+        $sql = "INSERT INTO {$table} (title,body)VALUES (:title,:body)";
         $stmt = $pdo->prepare($sql);
-        $stmt->execute(array(':val' => $content_str));
+        $stmt->execute(array(':title' => $title,':body' => $content_str));
         echo $pdo->lastinsertid();
     }
 }
